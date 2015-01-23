57545d47190873bf5e6b2d27c4c72ea056b381e6
flpa <flpa.dev@zoho.com>
2015-01-23 19:25:06 +0100

    indentation

--- tree.lisp
+++ tree.lisp
@@ -1,14 +1,21 @@
 (in-package :com.github.flpa.cl-tree)
 
+;; TODO: should be uppercased?
 (defparameter *show-hidden* nil)
+(defconstant +indent+ 4)
 
 ; test needs general way of getting folder or filename: pathname-name only returns
 ; file names?
-(walk-directory "/tmp/a"
+
+(let ((indent (- 0 +indent+))) ;; TODO: hacky or good?
+  (walk-directory "/tmp/a"
 			 #'(lambda (x)
 			     (if (directory-pathname-p x)
-				 (format t "das dir ~a~%" x)
-				 (format t "├── ~a~%"
-				     (pathname-name x)
-				     )))
-			 :directories t)
+				 (progn
+				   (incf indent 4)
+				   (format t "~vTdas dir ~a~%" indent x))
+				 (format t "~vT├── ~a~%"
+					 indent
+					 (pathname-name x)
+					 )))
+			 :directories t))

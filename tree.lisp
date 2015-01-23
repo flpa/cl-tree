4ba822a0a2ef724a254ada39ab6b94cb0e75fe51
flpa <flpa.dev@zoho.com>
2015-01-23 19:16:05 +0100

    drafting

--- tree.lisp
+++ tree.lisp
@@ -1,1 +1,14 @@
 (in-package :com.github.flpa.cl-tree)
+
+(defparameter *show-hidden* nil)
+
+; test needs general way of getting folder or filename: pathname-name only returns
+; file names?
+(walk-directory "/tmp/a"
+			 #'(lambda (x)
+			     (if (directory-pathname-p x)
+				 (format t "das dir ~a~%" x)
+				 (format t "├── ~a~%"
+				     (pathname-name x)
+				     )))
+			 :directories t)

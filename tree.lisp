73d0ed65df0b85d0c8680c9d4d43cf71f67b8306
flpa <flpa.dev@zoho.com>
2015-01-24 12:30:45 +0100

    tried building correct dir prefixes

--- tree.lisp
+++ tree.lisp
@@ -14,10 +14,13 @@
 
 (in-package :com.github.flpa.cl-tree)
 
+;; TODO: real tree sorts files alphabetically ignoring leading dots?
+
 ;; TODO: should be uppercased?
 (defparameter *show-hidden* nil)
 (defconstant +indent+ 4)
 (defconstant +line-middle+ "├── ")
+(defconstant +line-straight+ "│   ")
 (defconstant +line-end+ "└── ")
 
 (defun base-name (p)
@@ -27,15 +30,18 @@
 
 (defun walk-directory2 (dirname) 
   (labels
-      ((walk (name prefix)
-	 (princ prefix)
+      ((walk (name prefixes)
+	 (dolist (prefix prefixes) (princ prefix))
 	 (princ (base-name name))
 	 (terpri)
 	 (when (directory-pathname-p name)
+	   (let ((new-prefixes (if prefixes
+				   (append `(,+line-straight+) prefixes)
+				   `(,+line-middle+))))
 	    (dolist (x (list-directory name))
-	      (walk x (concatenate 'string prefix +line-middle+))))))
+	      (walk x new-prefixes))))))
     (fresh-line)
-    (walk (pathname-as-directory dirname) "")))
+    (walk (pathname-as-directory dirname) nil)))
 
 (walk-directory2 "/tmp/a")
 

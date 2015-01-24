87fdc733c788887a05a43d3c36f885733f2fa31e
flpa <flpa.dev@zoho.com>
2015-01-24 12:12:11 +0100

    rewrote using custom walk-directory2

--- tree.lisp
+++ tree.lisp
@@ -20,40 +20,27 @@
 (defconstant +line-middle+ "├── ")
 (defconstant +line-end+ "└── ")
 
-; test needs general way of getting folder or filename: pathname-name only returns
-; file names?
-
-
-;; how to peek for correctly printing last
-
-;;missing file ext?
-
-;; push dirs on stack ? does that help?
-
 (defun base-name (p)
   (or (and (directory-pathname-p p)
 	   (car (last (pathname-directory p))))
       (pathname-name p)))
 
-(let ((dir "/tmp/a") ;; will be param
-      (isfirst t)
-      (top-length nil))
-  (fresh-line)
-  (walk-directory dir
-		  #'(lambda (x)
-		      (if isfirst 
-			  (or (format t " .~%")
-			      (setf isfirst nil)
-			      (setf top-length (length (pathname-directory x))))
-			  (progn
-			    (format t "~vT~a~a~%"
-				    (* +indent+ (- (length (pathname-directory x))
-						   top-length
-						   (if (directory-pathname-p x) 1 0)))
-				    +line-middle+
-				    (base-name x)))))
-		    :directories t
-		    :test #'(lambda (x) (or *show-hidden*
-					    (not (char-equal #\.
-							     (aref (base-name x) 0)))))
-    ))
+(defun walk-directory2 (dirname) 
+  (labels
+      ((walk (name prefix)
+	 (princ prefix)
+	 (princ (base-name name))
+	 (terpri)
+	 (when (directory-pathname-p name)
+	    (dolist (x (list-directory name))
+	      (walk x (concatenate 'string prefix +line-middle+))))))
+    (fresh-line)
+    (walk (pathname-as-directory dirname) "")))
+
+(walk-directory2 "/tmp/a")
+
+(defun is-hidden (pathname)
+  (or *show-hidden*
+      (not (char-equal #\.
+		       (aref (base-name pathname) 0)))))
+

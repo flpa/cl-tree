ea9e64b38634a4850047d3d6f4d8835ddab1481d
flpa <flpa.dev@zoho.com>
2015-02-06 15:10:56 +0100

    Add file with sample commands for generating SBCL code coverage.

---
+++ coverage.lisp
@@ -0,0 +1,25 @@
+;;;; cl-tree: Reimplementing Unix 'tree' command in Common Lisp
+;;;; Copyright (C) 2015 Florian Patzl 
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;;; GNU General Public License for more details.
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;;; -----------------------------------------------------------------------------------------------
+
+;;; Example session for generating test coverage, copied from the SBCL manual and adapted.
+
+(require :sb-cover)
+(declaim (optimize sb-cover:store-coverage-data))
+(asdf:oos 'asdf:load-op :cl-tree/test :force t)
+
+(5am:run! 'com.github.flpa.cl-tree.test.suites:all)
+(sb-cover:report "/tmp/report/")
+
+(declaim (optimize (sb-cover:store-coverage-data 0)))
